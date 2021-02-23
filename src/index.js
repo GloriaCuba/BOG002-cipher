567318646084ab599c63bffcd0692df8f56fd42d
gloriacuba <gloriajiss@gmail.com>
2021-02-23 13:20:01 -0500

    primer cambio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
+//console.log(cipher);
 
-console.log(cipher);
+//var msm = prompt("Ingresa el código");
+var msm =document.getElementById(msmin); 
+console.log(msm) ;
+document.write(msm);
