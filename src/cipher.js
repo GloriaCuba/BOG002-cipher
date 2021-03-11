611174cf5729501f544b41964a89d66d18159de1
gloriacuba <gloriajiss@gmail.com>
2021-03-11 10:48:35 -0500

    primera prueba para trabajar el código en visual

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,50 @@
+//cipher es un objeto
 const cipher = {
-  // ...
-};
+  cifrar,descifrar,
+}
+function cifrar(msmUsuario, desplaz) {
+  let string = msmUsuario;
+  let offset = desplaz;
+  let theChain = string.length - 1;
+
+  var nuevaCadena = ""
+
+  for (let recor = 0; recor <= theChain; recor++) {
+    let ascii = string.charCodeAt(recor);
+    let suma = ascii + offset;
+    //if(suma<65){
+     // suma =32;
+    //  }
+    let msmCodif = String.fromCharCode(suma)
+    //console.log(msmCodif)
+    nuevaCadena = nuevaCadena + msmCodif;
+    
+  }
+  console.log(nuevaCadena);
+  return nuevaCadena;
+}
+
+function descifrar(msmUsuario2, desplaz2) {
+  let string = msmUsuario2;
+  let offset = desplaz2;
+  let theChain = string.length - 1;
+
+  var nuevaCadena = ""
+
+  for (let recor = 0; recor <= theChain; recor++) {
+    let ascii = string.charCodeAt(recor);
+    let suma = ascii - offset;
+    //if(suma<65){
+   //   suma =32;
+   // }
+      
+    let msmCodif = String.fromCharCode(suma)
+    console.log(msmCodif)
+    nuevaCadena = nuevaCadena + msmCodif;
+    
+  }
+  console.log(nuevaCadena);
+  return nuevaCadena;
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
-//import cipher from './cipher.js';
-//console.log(cipher);
+import cipher from './cipher.js';
 
-//var msm = prompt("Ingresa el código");
-var msm =document.getElementById(msmin); 
-console.log(msm) ;
-document.write(msm);
+const btninicio1 = document.getElementById("btniniciocifrar");
+btninicio1.addEventListener("click", function () {
+  mostrarcifrar()
+})
+const btninicio2 = document.getElementById("btniniciodescif");
+btninicio2.addEventListener("click", function () {
+  mostrardescifrar()
+})
+
+const btncopiar= document.querySelector(".btncopiar");
+const resultado= document.querySelector(".resultado");
+btncopiar.addEventListener("click", function () {
+  resultado.focus();
+  document.execCommand("selectAll");
+  document.execCommand("copy"); 
+})
+
+const btncopiar2= document.querySelector(".btncopiar2");
+const resultado2= document.querySelector(".resultado2");
+btncopiar2.addEventListener("click", function () {
+  resultado2.focus();
+  document.execCommand("selectAll");
+  document.execCommand("copy"); 
+})
+
+
+function mostrarcifrar() { 
+  btninicio.style.display = "none";
+ descripcion.style.display="none";
+ divcifrar.style.display = "block";
+  
+}
+
+function mostrardescifrar() {
+  btninicio.style.display = "none";
+  descripcion.style.display="none";
+  divdescif.style.display = "block";
+  
+}
+
+const btncif = document.getElementById("btncif");
+btncif.addEventListener("click", x);
+
+const btndescif = document.getElementById("btndescif");
+btndescif.addEventListener("click", y);
+
+function x() {
+  event.preventDefault()
+  let msmUsuario = document.getElementById("msm").value;
+  let clave = parseInt(document.getElementById("clave").value);
+
+  let msmCifrado = cipher.cifrar(msmUsuario, clave);
+  //console.log(msmCifrado);
+  document.getElementById("resultado1").innerHTML = msmCifrado;
+  console.log(cipher);
+
+}
+
+function y() {
+  event.preventDefault()
+  let msmUsuario2 = document.getElementById("msm2").value;
+  let clave2 = parseInt(document.getElementById("clave2").value);
+
+  let msmCifrado2 = cipher.descifrar(msmUsuario2, clave2);
+  document.getElementById("resultado2").innerHTML = msmCifrado2;
+  console.log(cipher);
+}
